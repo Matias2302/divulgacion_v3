9279aa67d428c07a30e60c1cc334049d7831df32
Matias2302 <matias.23.02@gmail.com>
2024-04-19 17:41:30 -0300

    probando slider de contenido

--- js/main.js
+++ js/main.js
@@ -44,3 +44,58 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+    } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+        } 
+    });
+}
+
+//SLIDERS
+const slides = document.querySelectorAll(".slide-card");
+console.log(slides);
+let slideIndex = 0;
+let intervalID = null;
+
+document.addEventListener("DOMContentLoaded", initializeSlider());
+
+function initializeSlider(){
+    if(slides.length > 0){
+        slides[slideIndex].classList.add("displaySlide");
+        intervalID = setInterval(nextSlide, 5000);
+    }
+}
+
+function showSlide(index){
+if(index >= slides.length){
+    slideIndex = 0;
+}
+else if(index < 0){
+    slideIndex = slides.length -1;
+}
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    console.log(slideIndex);
+    slides[slideIndex].classList.add("displaySlide");
+}
+
+function prevSlide(){
+    clearInterval(intervalID);
+    slideIndex--;
+    showSlide(slideIndex);
+}
+
+function nextSlide(){
+    slideIndex++;
+    showSlide(slideIndex);
+}
